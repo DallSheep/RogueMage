2a87dbc687d55bcdf20acf579a51ed59af31e1c9
christian dall <Christian.j.dall@gmail.com>
2023-11-20 14:42:43 -0500

    fix

--- RougeMage/Assets/Scripts/PlayerController.cs
+++ RougeMage/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerManager : MonoBehaviour, IDamage
+public class PlayerController : MonoBehaviour, IDamage
 {
     // Start is called before the first frame update
     void Start()
@@ -12,6 +12,11 @@ public class PlayerManager : MonoBehaviour, IDamage
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    public void takeDamage(int amount)
     {
         
     }
