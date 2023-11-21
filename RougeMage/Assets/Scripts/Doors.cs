afef543c19d60fe88039d9a3f1943a484b197414
Damien Meadows <KEMeadows@student.fullsail.edu>
2023-11-20 23:12:59 -0500

    Door Animation

--- RougeMage/Assets/Scripts/Doors.cs
+++ RougeMage/Assets/Scripts/Doors.cs
@@ -1,12 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class Doors : MonoBehaviour
 {
-    public void Open()
+    [SerializeField] Animator doorAnimation;
+    [SerializeField] TMP_Text lockedDoor;
+    [SerializeField] TMP_Text unlockedDoor;
+
+    bool isLocked;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            doorAnimation.Play("DoorOpen", 0, 0.0f);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            doorAnimation.Play("DoorClose", 0, 0.0f);
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
     }
 }
