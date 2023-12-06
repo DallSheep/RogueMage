928cf6af3883a0e549f13e71773634e2035c0879
christian dall <Christian.j.dall@gmail.com>
2023-12-06 12:11:05 -0500

    UI pt3

--- RougeMage/Assets/Scripts/ButtonFunction.cs
+++ RougeMage/Assets/Scripts/ButtonFunction.cs
@@ -43,15 +43,75 @@ public class ButtonFunction : MonoBehaviour
         SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void BuyMe()
+    //Items
+    public void HeroesHeart()
     {
-       
         if (GameManager.Instance.playerScript.gold >= 25)
         {
            //GameManager.Instance.heroesHeart.GetComponentInChildren<TMP_Text>().enabled = true;
            GameManager.Instance.playerScript.gold -= 25;
-           GameManager.Instance.playerScript.Hp += (GameManager.Instance.playerScript.Hp / 4);
+           GameManager.Instance.playerScript.Hp += (int)(GameManager.Instance.playerScript.Hp / 4);
             
+        }
+    }
+
+    public void PlasmaBall()
+    {
+        if (GameManager.Instance.playerScript.gold >= 25)
+        {
+            GameManager.Instance.playerScript.gold -= 25;
+            GameManager.Instance.playerScript.currMana += (int)(GameManager.Instance.playerScript.currMana / 4);
+        }
+    }
+
+    public void DragonsAle() 
+    {
+        if (GameManager.Instance.playerScript.gold >= 25)
+        {
+            GameManager.Instance.playerScript.gold -= 25;
+            GameManager.Instance.playerScript.stamina += 100;
+        }
+    }
+
+    public void DwarvenRing() 
+    {
+        if (GameManager.Instance.playerScript.gold >= 25)
+        {
+            GameManager.Instance.playerScript.gold -= 25;
+
+        }
+    }
+
+    //Spells
+    public void FireFlare()
+    {
+        if (GameManager.Instance.playerScript.gold >= 100)
+        {
+            GameManager.Instance.playerScript.gold -= 100;
+        }
+    }
+
+    public void ElectricCharge()
+    {
+        if (GameManager.Instance.playerScript.gold >= 100)
+        {
+            GameManager.Instance.playerScript.gold -= 100;
+        }
+    } 
+    
+    public void RockCatapult()
+    {
+        if (GameManager.Instance.playerScript.gold >= 100)
+        {
+            GameManager.Instance.playerScript.gold -= 100;
+        }
+    }
+    
+    public void JetStream()
+    {
+        if (GameManager.Instance.playerScript.gold >= 100)
+        {
+            GameManager.Instance.playerScript.gold -= 100;
         }
     }
 }
