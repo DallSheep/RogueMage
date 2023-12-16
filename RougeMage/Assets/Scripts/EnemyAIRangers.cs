be5100cd64d700ed8f1a87c6367860fb02d74e1b
LogMoy32 <123963485+LogMoy32@users.noreply.github.com>
2023-12-16 11:21:06 -0500

    enemies tinkered with

--- RougeMage/Assets/Scripts/EnemyAIRangers.cs
+++ RougeMage/Assets/Scripts/EnemyAIRangers.cs
@@ -75,11 +75,11 @@ public class EnemyAIRangers : MonoBehaviour, IDamage
 
             if (playerInRange && !canSeePlayer())
             {
-                StartCoroutine(roam());
+                //StartCoroutine(roam());
             }
             else if (!playerInRange)
             {
-                StartCoroutine(roam());
+                //StartCoroutine(roam());
             }
         }
     }
